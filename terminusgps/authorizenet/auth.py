69456ffe93867f9d065bf4e1d842906fa1c5a0b8
darthnall <blake@proton.me>
2024-12-13 10:21:31 -0600

    Added django and updated authorizenet.auth.get_merchant_auth

--- terminusgps/authorizenet/auth.py
+++ terminusgps/authorizenet/auth.py
@@ -1,14 +1,15 @@
-from os import getenv
-
 from authorizenet.apicontractsv1 import merchantAuthenticationType
 from authorizenet.constants import constants
 
+from django.conf import settings
+
 
 def get_merchant_auth() -> merchantAuthenticationType:
-    name: str = getenv("MERCHANT_AUTH_LOGIN_ID", "")
-    transactionKey: str = getenv("MERCHANT_AUTH_TRANSACTION_KEY", "")
-    return merchantAuthenticationType(name=name, transactionKey=transactionKey)
+    return merchantAuthenticationType(
+        name=settings.MERCHANT_AUTH_LOGIN_ID,
+        transactionKey=settings.MERCHANT_AUTH_TRANSACTION_KEY,
+    )
 
 
-def get_environment(debug: bool = False) -> str:
-    return constants.SANDBOX if debug else constants.PRODUCTION
+def get_environment() -> str:
+    return constants.SANDBOX if settings.DEBUG else constants.PRODUCTION
