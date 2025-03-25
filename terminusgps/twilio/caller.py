de8f96f99557d6cd0e8725155918ae2879efaac4
darthnall <blake@proton.me>
2025-03-25 13:15:44 -0500

    Removed logging

--- terminusgps/twilio/caller.py
+++ terminusgps/twilio/caller.py
@@ -1,25 +1,31 @@
 import asyncio
-import logging
 from typing import Any
 
 import twilio.rest
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from twilio.http.async_http_client import AsyncTwilioHttpClient
 
-from terminusgps.twilio.logger import TwilioLogger
+if not hasattr(settings, "TWILIO_SID"):
+    raise ImproperlyConfigured("'TWILIO_SID' setting is required.")
+if not hasattr(settings, "TWILIO_TOKEN"):
+    raise ImproperlyConfigured("'TWILIO_TOKEN' setting is required.")
+if not hasattr(settings, "TWILIO_FROM_NUMBER"):
+    raise ImproperlyConfigured("'TWILIO_FROM_NUMBER' setting is required.")
+if not hasattr(settings, "TWILIO_MESSAGING_SID"):
+    raise ImproperlyConfigured("'TWILIO_MESSAGING_SID' setting is required.")
 
 
 class TwilioCaller:
-    def __init__(self, log_level: int = logging.CRITICAL) -> None:
+    def __init__(self) -> None:
+        """Sets Twilio messaging session variables."""
         self.client_sid = settings.TWILIO_SID
         self.client_token = settings.TWILIO_TOKEN
         self.from_number = settings.TWILIO_FROM_NUMBER
         self.messaging_sid = settings.TWILIO_MESSAGING_SID
-        self.logger = TwilioLogger(
-            logging.getLogger(self.__class__.__name__), level=log_level
-        ).get_logger()
 
     def __enter__(self) -> "TwilioCaller":
+        """Creates an asyncronous Twilio client."""
         self.client = twilio.rest.Client(
             self.client_sid, self.client_token, http_client=AsyncTwilioHttpClient()
         )
@@ -31,10 +37,21 @@ class TwilioCaller:
     async def create_notification(
         self, to_number: str, message: str, method: str = "sms"
     ) -> asyncio.Task[Any]:
-        self.logger.debug(
-            f"Creating '{method}' notification targeting '{to_number}'..."
-        )
+        """
+        Returns an awaitable notification task.
 
+        Available methods are ``"sms"``, ``"call"`` and ``"phone"``.
+
+        :param to_number: A phone number to notify.
+        :type to_number: :py:obj:`str`
+        :param message: A notification message.
+        :type message: :py:obj:`str`
+        :param method: A notification method. Default is ``"sms"``.
+        :type method: :py:obj:`str`
+        :returns: An awaitable task.
+        :rtype: :py:obj:`~asyncio.Task`
+
+        """
         match method:
             case "sms":
                 return asyncio.create_task(
@@ -44,12 +61,21 @@ class TwilioCaller:
                 return asyncio.create_task(
                     self.create_call(to_number=to_number, message=message)
                 )
-            case "log":
-                return asyncio.create_task(self.create_log(message=message))
             case _:
                 raise ValueError(f"Unsupported TwilioCaller method '{method}'.")
 
     async def create_call(self, to_number: str, message: str) -> None:
+        """
+        Calls ``to_number`` and reads ``message`` aloud.
+
+        :param to_number: A phone number.
+        :type to_number: :py:obj:`str`
+        :param message: A message to be read aloud.
+        :type message: :py:obj:`str`
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
         await self.client.calls.create_async(
             to=to_number,
             from_=self.from_number,
@@ -57,12 +83,20 @@ class TwilioCaller:
         )
 
     async def create_sms(self, to_number: str, message: str) -> None:
+        """
+        Texts ``message`` to ``to_number``.
+
+        :param to_number: A phone number.
+        :type to_number: :py:obj:`str`
+        :param message: A message to be texted.
+        :type message: :py:obj:`str`
+        :returns: Nothing.
+        :rtype: :py:obj:`None`
+
+        """
         await self.client.messages.create_async(
             to=to_number,
             from_=self.from_number,
             body=message,
             messaging_service_sid=self.messaging_sid,
         )
-
-    async def create_log(self, message: str) -> None:
-        self.logger.info(message)
