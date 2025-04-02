9328a30e73eee305f8949b3cc8a1efda24281353
darthnall <blake@proton.me>
2025-04-02 11:39:16 -0500

    Fixed bug

--- terminusgps/aws/connect.py
+++ terminusgps/aws/connect.py
@@ -1,16 +1,23 @@
-import jwt
 import datetime
 
+import jwt
 from django.conf import settings
 
+
 def get_aws_connect_jwt(widget_id: str, expires_in: int = 500) -> str:
-    payload = {
-        "sub": widget_id,
-        "iat": datetime.datetime.utcnow(),
-        "exp": datetime.datetime.utcnow() - datetime.timedelta(seconds=expires_in),
-        "customerId": "",
-        "segmentAttributes": {"connect:Subtype": {"ValueString" : "connect:Guide"}}, 'attributes': {"name": "Jane", "memberID": "123456789", "email": "Jane@example.com", "isPremiumUser": "true", "age": "45"} },
-    }
+    if expires_in > 600:
+        raise ValueError(f"'expires_in' must be less than 600, got '{expires_in}'.")
+
+    payload = (
+        {
+            "sub": widget_id,
+            "iat": datetime.datetime.utcnow(),
+            "exp": datetime.datetime.utcnow() - datetime.timedelta(seconds=expires_in),
+            "segmentAttributes": {"connect:Subtype": {"ValueString": "connect:Guide"}},
+        },
+    )
     header = {"typ": "JWT", "alg": "HS256"}
-    encoded_token = jwt.encode((payload), settings.CONNECT_SECRET, algorithm=header["alg"], headers=header)
+    encoded_token = jwt.encode(
+        (payload), settings.CONNECT_SECRET, algorithm=header["alg"], headers=header
+    )
     return encoded_token
