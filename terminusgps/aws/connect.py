ef5b2fe71e4b52804e89083cdae03ba414436c6e
darthnall <blake@proton.me>
2025-04-02 11:43:36 -0500

    Fixed aws jwt connect generation again

--- terminusgps/aws/connect.py
+++ terminusgps/aws/connect.py
@@ -8,14 +8,12 @@ def get_aws_connect_jwt(widget_id: str, expires_in: int = 500) -> str:
     if expires_in > 600:
         raise ValueError(f"'expires_in' must be less than 600, got '{expires_in}'.")
 
-    payload = (
-        {
-            "sub": widget_id,
-            "iat": datetime.datetime.utcnow(),
-            "exp": datetime.datetime.utcnow() - datetime.timedelta(seconds=expires_in),
-            "segmentAttributes": {"connect:Subtype": {"ValueString": "connect:Guide"}},
-        },
-    )
+    payload = {
+        "sub": widget_id,
+        "iat": datetime.datetime.utcnow(),
+        "exp": datetime.datetime.utcnow() - datetime.timedelta(seconds=expires_in),
+        "segmentAttributes": {"connect:Subtype": {"ValueString": "connect:Guide"}},
+    }
     header = {"typ": "JWT", "alg": "HS256"}
     encoded_token = jwt.encode(
         payload, settings.CONNECT_SECRET, algorithm=header["alg"], headers=header
