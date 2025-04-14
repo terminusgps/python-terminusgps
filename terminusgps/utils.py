3957ffde4e2e1b58a9dfb856fd8599cb3a596100
darthnall <blake@proton.me>
2025-04-14 14:48:43 -0500

    Fixed utility function

--- terminusgps/utils.py
+++ terminusgps/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
+from PIL.Image import Image
 from pyzbar.pyzbar import decode
 
 
-def scan_barcode(img: np.ndarray) -> list:
+def scan_barcode(img: np.ndarray | Image) -> list:
     return decode(img)
