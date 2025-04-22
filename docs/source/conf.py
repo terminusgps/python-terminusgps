19f609b0f68b475ccfca525cfbc9b2acafb2f62a
darthnall <blake@proton.me>
2025-04-22 11:26:44 -0500

    Rebuilt docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ django.setup()
 project = "python-terminusgps"
 copyright = "2025, Terminus GPS, LLC"
 author = "Terminus GPS, LLC"
-release = "31.7.1"
+release = "31.7.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
