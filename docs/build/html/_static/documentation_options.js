314ef3b6e2f768e8b126a116dc7ed11fcb35cd4e
darthnall <blake@proton.me>
2025-05-02 11:51:09 -0500

    Rebuilt ddocs

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '35.5.0',
+    VERSION: '35.6.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ django.setup()
 project = "python-terminusgps"
 copyright = "2025, Terminus GPS, LLC"
 author = "Terminus GPS, LLC"
-release = "35.5.0"
+release = "35.6.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
