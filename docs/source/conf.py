c3327a8b32575acf94e5847bb3ad6a977b20ac3a
darthnall <blake@proton.me>
2025-10-06 13:35:17 -0500

    Bumped docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath("../../"))
 project = "python-terminusgps"
 copyright = "2025, Terminus GPS, LLC"
 author = "Terminus GPS, LLC"
-release = "46.1.0"
+release = "47.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
